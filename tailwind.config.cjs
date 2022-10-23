d0c32e34e08c194b5156c28ca4b15b0e3af5b155
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-10-22 21:23:46 -0400

    set up base css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'dark-orchid': 'hsl(263, 55%, 52%)',
+        dusk: 'hsl(217, 19%, 35%)',
+        dark: 'hsl(219, 29%, 14%)',
+        'dark-gray': 'hsl(217, 19%, 35%)',
+        'light-gray': 'hsl(210, 46%, 95%)',
+        'medium-gray': 'hsl(0, 0%, 81%)',
+      },
+    },
   },
   plugins: [],
 };
